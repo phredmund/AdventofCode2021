3095c15c80e81e7debe29b82e13661be2cca8215
phredmund <eexley@gmail.com>
2021-12-01 11:28:07 -0500

    Initial commit

---
+++ Day_01/main.py
@@ -0,0 +1,44 @@
+def load_input():
+    depths = []
+    with open("./input.txt", "r") as file:
+        for line in file:
+            depths.append(int(line.strip()))
+    return depths
+
+
+def part1():
+    data = load_input()
+    counter = 0
+    depth1 = data[0]
+    for depth in data:
+        if depth > depth1:
+            counter = counter + 1
+        depth1 = depth
+    print(f"Part 1: {counter}")
+
+
+def part2():
+    data = load_input()
+    counter = 0
+    start = 1
+    end = 4
+    total1 = data[0] + data[1] + data[3]
+    while end <= len(data):
+        total = 0
+        for num in data[start:end]:
+            total = total + num
+        if total > total1:
+            counter = counter + 1
+        total1 = total
+        start = start + 1
+        end = end + 1
+    print(f"Part 2: {counter}")
+
+
+def main():
+    part1()
+    part2()
+
+
+if __name__ == '__main__':
+    main()
