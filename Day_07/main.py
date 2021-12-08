13fa6c04fb8d9b6174be580d49b08577a96543e7
phredmund <eexley@gmail.com>
2021-12-08 14:17:56 -0500

    Day 7, part 2. This takes too long.

--- Day_07/main.py
+++ Day_07/main.py
@@ -3,12 +3,27 @@ def get_input():
         return list(map(int, file.readline().split(",")))
 
 
+def fuel_calc(x):
+    if x <= 1:
+        return x
+    else:
+        total = x
+        while x > 1:
+            total += (x-1)
+            x -= 1
+        return total
+
+
 def main():
     crabs = get_input()
     moves = []
+    moves2 = []
     for position in range(max(crabs)):
         moves.append(sum(list(map(lambda x: abs(x - position), crabs))))
-    print(f"Min moves: {min(moves)}")
+    print(f"Part 1: {min(moves)}")
+    for position in range(max(crabs)):
+        moves2.append(sum(list(map(lambda x: fuel_calc(abs(position - x)), crabs))))
+    print(f"Part 2: {min(moves2)}")
 
 
 if __name__ == '__main__':
